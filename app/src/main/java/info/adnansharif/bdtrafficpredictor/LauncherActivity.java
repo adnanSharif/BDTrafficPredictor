2470d4fd44194241db2191a7d3df902317b4f226
Adnan Sharif <adnan.sharif.mail@gmail.com>
2018-01-28 14:16:13 +0600

    Initial commit

---
+++ app/src/main/java/info/adnansharif/bdtrafficpredictor/LauncherActivity.java
@@ -0,0 +1,23 @@
+package info.adnansharif.bdtrafficpredictor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_launcher_activity);
+
+        Intent intent = new Intent(this.getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+
+    }
+}
